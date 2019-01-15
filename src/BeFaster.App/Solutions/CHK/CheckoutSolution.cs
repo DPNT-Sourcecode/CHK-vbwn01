136603cee7199a92f5d7fb0ad73408cc38a9b106
sbx_user1060 <sbx_user1060@ip-10-129-148-60.eu-west-2.compute.internal>
2019-01-15 01:36:45 +0000

    Tue Jan 15 01:36:45 UTC 2019

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,21 +1,46 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+        public static List<Item> ItemsList = InitItemsList();
+
+        public static List<Item> InitItemsList()
+        {
+            return new List<Item>()
+            {
+                new Item(){Sku = 'A', Price = 50, specialOffer = new SpecialOffer(){Amount = 3, Price = 130}},
+                new Item(){Sku = 'B', Price = 30, specialOffer = new SpecialOffer(){Amount = 2, Price = 45}},
+                new Item(){Sku = 'C', Price = 20, specialOffer = null},
+                new Item(){Sku = 'D', Price = 15, specialOffer = null}
+            };
+        }
+
         public static int Checkout(string skus)
         {
             if (!IsInputValid(skus))
             {
                 return -1;
             }
+            return -1;
         }
 
         public static bool IsInputValid(string skus)
         {
+            foreach(var sku in skus.Distinct())
+            {
+                bool isCharValid = ItemsList.Where(x => x.Sku == sku).FirstOrDefault() != null;
+                if (!isCharValid)
+                {
+                    return false;
+                }
+            }
             return true;
         }
     }
 }
+
 
