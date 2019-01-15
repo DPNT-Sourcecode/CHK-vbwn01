3a18561fba89781e4d90597d12258035cebb0bcb
sbx_user1060 <sbx_user1060@ip-10-129-148-60.eu-west-2.compute.internal>
2019-01-15 02:00:45 +0000

    Tue Jan 15 02:00:45 UTC 2019

---
+++ src/BeFaster.App/Solutions/CHK/ProductAmountOrdered.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public class ProductAmountOrdered
+    {
+        public char Sku;
+        public int Amount;
+        public int Price;
+    }
+}
