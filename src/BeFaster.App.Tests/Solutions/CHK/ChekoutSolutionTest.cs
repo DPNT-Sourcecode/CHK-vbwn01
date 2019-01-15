a41d09ea15b579f50c874e65a4cc86842e2e9e61
sbx_user1060 <sbx_user1060@ip-10-129-148-60.eu-west-2.compute.internal>
2019-01-15 01:54:45 +0000

    Tue Jan 15 01:54:45 UTC 2019

--- src/BeFaster.App.Tests/Solutions/CHK/ChekoutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/ChekoutSolutionTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using BeFaster.App.Solutions.CHK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Tests.Solutions.CHK
 {
-    class ChekoutSolutionTest
+    [TestFixture]
+    public class ChekoutSolutionTest
     {
+        [TestCase("")]
+        [TestCase("!")]
+        [TestCase(" ")]
+        [TestCase("2")]
+        public void ReturnFalseGivenStringContainsInvalidCharacters(string skus)
+        {
+            bool isValid = CheckoutSolution.IsInputValid(skus);
+            Assert.IsFalse(isValid, $"{skus} should not be valid");
+        }
     }
 }
+
